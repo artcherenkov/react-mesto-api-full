196e7aa767066d5e46aac87bb750ebfbb348cf3f
Artem Cherenkov <art.cherenkov@gmail.com>
2021-08-01 07:35:59 +0000

    feat: добавить краш-тест сервера

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,11 +6,19 @@ const { login, logout, createUser } = require('../controllers/users');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
+const crashTest = () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+};
+
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
 
 router.post('/signin', validateAuth, login);
 router.post('/signup', validateAuth, createUser);
 router.post('/logout', logout);
+
+router.get('/crash-test', crashTest);
 
 module.exports = router;
